faf6871a32d0ad1727da41f1d3638bfd2eb60a6f
aleksejs.marmiss <aleksejs.marmiss@gmail.com>
2021-04-26 21:17:35 +0200

    Fixed tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render main', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Awsome Xeelver UI/i);
   expect(linkElement).toBeInTheDocument();
 });
